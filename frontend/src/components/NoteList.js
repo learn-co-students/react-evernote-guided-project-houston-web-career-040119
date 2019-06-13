5cdaa37c37112ef64034e93953fc3c6cb05becb3
Thomas_Hansen <ctrhansen@yahoo.com>
2019-06-13 13:59:08 -0500

    Data

--- frontend/src/components/NoteList.js
+++ frontend/src/components/NoteList.js
@@ -4,8 +4,8 @@ import NoteItem from './NoteItem';
 const NoteList = (props) => {
   return (
     <ul>
-      {/* Render list of notes here... */}
-      <NoteItem />
+      {props.notes.map(note => <NoteItem note={note}/>)}
+      
     </ul>
   );
 }
