8910deb909ae26b352589100b99f164e379a52ae
github username <github email address>
2019-06-13 16:21:35 -0500

    hello

--- frontend/src/components/NoteList.js
+++ frontend/src/components/NoteList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import NoteItem from './NoteItem';
 
+
 const NoteList = (props) => {
+  console.log(props)
   return (
-    <ul>
-      {props.notes.map(note => <NoteItem note={note}/>)}
-      
-    </ul>
+    <div>
+       {props.notes.map(note=><NoteItem note={note}/>)}
+    </div>
   );
 }
 
