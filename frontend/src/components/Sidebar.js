c8d1195d9534fcd21e8fc717152eb9ceee1bdf1c
github username <github email address>
2019-06-13 17:20:26 -0500

    anychanges

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import NoteList from './NoteList';
 
 class Sidebar extends Component {
+  constructor(){
+    super()
+    this.state = {
+      isToggleOn: false
+    }
+  }
+
+  handleClick(){
+    this.setState({
+      isToggleOn: !state.isToggleOn
+    })
+  }
+  
   render() {
     // console.log(this.props.notes)
     return (
       <div className='master-detail-element sidebar'>
         <NoteList notes={this.props.notes}/>
-        <button>New</button>
+        <button onClick>New</button>
       </div>
     );
   }
